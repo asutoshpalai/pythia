d37e436dee868085927762b38f0f4a1e6e607205
Asutosh Palai <asupalai@gmail.com>
2016-04-11 08:34:06 +0530

    Added kernel_entry asm

---
+++ kernel_entry.s
@@ -0,0 +1,8 @@
+; Ensures that we jump straight into the kernel ’s entry function.
+[ bits 32 ] ; We ’re in protected mode by now , so use 32 - bit instructions.
+[ extern main  ] ; Declate that we will be referencing the external symbol ’main ’,
+; so the linker can substitute the final address
+
+call main
+
+jmp $
